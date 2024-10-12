c2ae63a007cfb39333b1d109843bde762bbf3bc2
panchaliSam <panchalisam20030117@gmail.com>
2024-10-12 18:07:08 +0530

    Folder structure

---
+++ CSSE_MediSync/src/main/java/servlet/TestConnectionServlet.java
@@ -0,0 +1,42 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import utility.DBConnection;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class TestConnectionServlet
+ */
+@WebServlet("/TestConnectionServlet")
+public class TestConnectionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestConnectionServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Connection connection = DBConnection.getConnection();
+
+	    if (connection != null) {
+	        response.getWriter().append("Database connected successfully!");
+	    } else {
+	        response.getWriter().append("Database connectivity failed!");
+	    }
+	}
+}
