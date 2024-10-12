32d1a6a75acc54bfb9c01b5b855fbb62e0a5601e
Kumodi Bogahawatte <kumodib@gmail.com>
2024-10-12 17:37:09 +0530

    Folder structure

---
+++ CSSE_MediSync/src/main/java/interfaces/IPatientDAO.java
@@ -0,0 +1,23 @@
+package interfaces;
+
+import java.util.List;
+
+import model.Patient;
+
+public interface IPatientDAO {
+	
+	//Retrieves all patients from database
+	public List<Patient> selectAllPatients();
+	
+	//Retrieves a specific patient based on their ID
+	public Patient selectPatient(int patientID);
+	
+	//Inserts a new patient record into the database
+	public void insertPatient(Patient patient);
+	
+	//Updates an existing patient record in the database
+	public void updatePatient(Patient patient);
+	
+	//Deletes a patient record from the database based on their ID
+	public void deletePatient(int patientID);
+}
